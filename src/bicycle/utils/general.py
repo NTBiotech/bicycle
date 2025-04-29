a7210387afbf39c695f5ec7741104ecdafd9714b
NTBiotech <nicolas.toulouse@stud.uni-heidelberg.de>
2025-04-29 18:01:35 +0200

    function to format more general log file names

--- src/bicycle/utils/general.py
+++ src/bicycle/utils/general.py
@@ -12,3 +12,19 @@ def get_full_name(
     swa,
 ):
     return f"{name_prefix}_{nlogo}_{seed}_{lr}_{n_genes}_{scale_l1}_{scale_kl}_{scale_spectral_loss}_{scale_lyapunov}_{gradient_clip_val}_{swa}"
+
+def get_custom_name(*args):
+    """
+    Function that converts all input variables into one string with values and one string with names.
+    Format: "valueofv1_valueofv2_..._valueofvx"
+
+    """
+    value_string = ""
+    key_string = ""
+    for key, value in globals().items():
+        if value in args:
+            value_string += f"{value}_"
+            key_string += f"{key}_"
+
+
+    return value_string[:-1], key_string[:-1]
