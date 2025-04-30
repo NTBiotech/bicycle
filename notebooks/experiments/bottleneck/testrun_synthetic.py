1123c2cdd23db880053ae6d0584770307806b686
NTBiotech <nicolas.toulouse@stud.uni-heidelberg.de>
2025-04-30 16:07:18 +0200

    Usable testrun_synthetic code for bottlenecking

--- notebooks/experiments/bottleneck/testrun_synthetic.py
+++ notebooks/experiments/bottleneck/testrun_synthetic.py
@@ -3,26 +3,22 @@ Test run of the bicycle model with synthetic data for bottleneck finding.
 
 Generates diagostics and output data in the data subdirectory.
 
-
-
 """
 
 import os
 import time
 from pathlib import Path
-import glob
 
 import numpy as np
 import pandas as pd
 import torch
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import RichProgressBar, StochasticWeightAveraging
-from pytorch_lightning.tuner.tuning import Tuner
-
 
 from bicycle.model import BICYCLE
-from bicycle.utils.data import create_data, create_loaders
-from bicycle.utils.general import get_custom_name, get_id
+from bicycle.utils.data import create_data, create_loaders, get_diagonal_mask
+from bicycle.utils.general import get_id
+from bicycle.utils.plotting import plot_training_results
 from bicycle.dictlogger import DictLogger
 from bicycle.callbacks import (
     CustomModelCheckpoint,
@@ -31,15 +27,28 @@ from bicycle.callbacks import (
 )
 
 SEED = 1
-pl.seed_everything(SEED)
-np.random.seed(SEED)
-
-torch.set_float32_matmul_precision("high")
-
 GPU_DEVICE = 0
 
+pl.seed_everything(SEED)
+torch.set_float32_matmul_precision("high")
+
+"""
+commonly used parameters:
+n_genes
+intervention_type
+SEED
+n_samples_control
+n_samples_per_perturbation
+batch_size
+n_epochs
+n_epochs_pretrain_latents
+mask
+validation_size
+"""
+
+
 # configure output paths
-## subdirectories model and plots 
+## subdirectories model and plots
 ## in subdirectory data of the current path
 OUT_PATH = Path(os.path.join(".", "notebooks/experiments/bottleneck/data"))
 MODELS_PATH = Path(os.path.join(OUT_PATH, "models"))
@@ -58,15 +67,16 @@ for directory in [MODELS_PATH, PLOTS_PATH]:
 # create synthetic data
 ## parameters for data creation with prefix data
 
-data_n_genes = 10
-data_n_samples_control = 500
-data_n_samples_per_perturbation = 250
+data_n_genes = 4 # needs to be a round number
+data_n_samples_control = 50
+data_n_samples_per_perturbation = 26
 data_make_counts=True
-data_train_gene_ko=np.arange(0, data_n_genes, 1).astype(str)
+data_train_gene_ko=list(np.arange(0, data_n_genes, 1).astype(str))
 data_test_gene_ko = []
 while len(data_test_gene_ko)<= 20:
-    data_test_gene_ko.append(tuple(np.random.choice(10, size=2, replace=False).astype(str)))
+    data_test_gene_ko.append(tuple(np.random.choice(data_n_genes, size=2, replace=False).astype(str)))
     data_test_gene_ko=list(set(data_test_gene_ko))
+data_test_gene_ko = [f"{x[0]},{x[1]}" for x in data_test_gene_ko]
 
 data_graph_type="erdos-renyi"
 data_edge_assignment="random-uniform"
@@ -110,19 +120,19 @@ np.save(os.path.join(MODELS_PATH, run_id, "check_sim_regimes.npy"), sim_regime)
 np.save(os.path.join(MODELS_PATH, run_id, "check_sim_beta.npy"), beta)
 np.save(os.path.join(MODELS_PATH, run_id, "check_sim_gt_interv.npy"), gt_interv)
 
+# TODO: create check for already existing runs
 
 # initialize data loaders
 validation_size = 0.2
 batch_size = 10_000
 
-train_loader, validation_loader, test_loader, _ = create_loaders(samples=samples,
+train_loader, validation_loader, test_loader = create_loaders(samples=samples,
                           sim_regime=sim_regime,
                           validation_size=validation_size,
                           batch_size=batch_size,
                           SEED= SEED,
                           train_gene_ko=data_train_gene_ko,
                           test_gene_ko=data_test_gene_ko,
-                          num_workers=1,
                           persistent_workers=False,
                           covariates=None,
 )
@@ -130,14 +140,15 @@ train_loader, validation_loader, test_loader, _ = create_loaders(samples=samples
 
 # initialize parameters for the bicycle class with prefix model
 model_lr = 1e-3
-model_gt_interv = None
-model_n_genes = data_n_genes
-model_n_samples = data_n_samples_control + (len(data_train_gene_ko) + len(data_test_gene_ko)) * data_n_samples_per_perturbation
+model_gt_interv = gt_interv
+model_n_genes = samples.shape[1]
+model_n_samples = samples.shape[0]
 model_lyapunov_penalty = True
 model_perfect_interventions = True
 model_rank_w_cov_factor = model_n_genes
 model_optimizer = "adam"
-model_optimizer_kwargs = {"betas": [0.5, 0.9]}  # Faster decay for estimates of gradient and gradient squared
+# Faster decay for estimates of gradient and gradient squared:
+model_optimizer_kwargs = {"betas": [0.5, 0.9]}
 model_device = torch.device(f"cuda:{GPU_DEVICE}")
 model_scale_l1 = 0.1
 model_scale_spectral = 1.0
@@ -150,12 +161,12 @@ model_early_stopping_p_mode = True
 model_x_distribution = "Multinomial"
 model_x_distribution_kwargs = {}
 model_init_tensors = None
-model_mask = None
+model_mask = get_diagonal_mask(n_genes=model_n_genes, device=model_device)
 model_use_encoder = False
 model_gt_beta = beta
 model_train_gene_ko = data_train_gene_ko
 model_test_gene_ko = data_test_gene_ko
-model_use_latents = True
+model_use_latents = True    # Should be the same as data_make_counts
 model_covariates = None
 model_n_factors = 0
 model_intervention_type = "Cas9"
@@ -165,6 +176,11 @@ model_learn_T = False
 model_train_only_likelihood = False
 model_train_only_latents = False
 model_mask_genes = []
+
+print("Training data:")
+print(f"- Number of training samples: {len(train_loader.dataset)}")
+if validation_size > 0:
+    print(f"- Number of validation samples: {len(validation_loader.dataset)}")
 
 model = BICYCLE(
     lr = model_lr,
@@ -209,9 +225,9 @@ model.to(model_device)
 
 # training variables
 
-n_epochs = 10
+n_epochs = 5100
 gradient_clip_val = 1.0
-plot_epoch_callback = 1
+plot_epoch_callback = 500 # intervall for plot_training_results -> GenerateCallback
 
 # callback variables
 swa = 250
@@ -231,42 +247,41 @@ loggers = [DictLogger()]
 callbacks = [
     RichProgressBar(refresh_rate=1),
     GenerateCallback(
-        MODELS_PATH.joinpath(run_id, "plotname"), 
-        plot_epoch_callback=plot_epoch_callback, 
+        MODELS_PATH.joinpath(run_id, "generatecallback", "during.png"),
+        plot_epoch_callback=plot_epoch_callback,
         true_beta=beta.cpu().numpy()
     ),
 ]
 if swa > 0:
-                    callbacks.append(StochasticWeightAveraging(0.01, swa_epoch_start=swa))
+    callbacks.append(StochasticWeightAveraging(0.01, swa_epoch_start=swa))
 
 if CHECKPOINTING:
-                    Path(os.path.join(MODELS_PATH, run_id)).mkdir(parents=True, exist_ok=True)
-                    callbacks.append(
-                        CustomModelCheckpoint(
-                            dirpath=os.path.join(MODELS_PATH, run_id),
-                            filename="{epoch}",
-                            save_last=True,
-                            save_top_k=1,
-                            verbose=VERBOSE_CHECKPOINTING,
-                            monitor="valid_loss",
-                            mode="min",
-                            save_weights_only=True,
-                            start_after=0,
-                            save_on_train_epoch_end=False,
-                            every_n_epochs=1,
-                        )
-                    )
-                    callbacks.append(MyLoggerCallback(dirpath=os.path.join(MODELS_PATH, run_id)))
+    callbacks.append(
+        CustomModelCheckpoint(
+            dirpath=os.path.join(MODELS_PATH, run_id, "customcheckpoint"),
+            filename="{epoch}",
+            save_last=True,
+            save_top_k=1,
+            verbose=VERBOSE_CHECKPOINTING,
+            monitor="valid_loss",
+            mode="min",
+            save_weights_only=True,
+            start_after=0,
+            save_on_train_epoch_end=False,
+            every_n_epochs=1,
+        )
+    )
+    callbacks.append(MyLoggerCallback(dirpath=os.path.join(MODELS_PATH, run_id, "mylogger")))
 
 # pretraining
-n_epochs_pretrain_latents = 2
+n_epochs_pretrain_latents = 100
 
 if model.use_latents and n_epochs_pretrain_latents > 0:
 
     pretrain_callbacks = [
         RichProgressBar(refresh_rate=1),
         GenerateCallback(
-            str(MODELS_PATH.joinpath(id, "_pretrain")),
+            str(MODELS_PATH.joinpath(run_id, "generatecallback","pretrain_during.png")),
             plot_epoch_callback=plot_epoch_callback,
             true_beta=beta.cpu().numpy(),
         ),
@@ -275,7 +290,7 @@ if model.use_latents and n_epochs_pretrain_latents > 0:
     if swa > 0:
         pretrain_callbacks.append(StochasticWeightAveraging(0.01, swa_epoch_start=swa))
 
-    pretrain_callbacks.append(MyLoggerCallback(dirpath=os.path.join(MODELS_PATH, run_id)))
+    pretrain_callbacks.append(MyLoggerCallback(dirpath=os.path.join(MODELS_PATH, run_id, "mylogger")))
 
     pretrainer = pl.Trainer(
         max_epochs=n_epochs_pretrain_latents,
@@ -300,9 +315,9 @@ if model.use_latents and n_epochs_pretrain_latents > 0:
     pretrainer.fit(model, train_loader, validation_loader)
     end_time = time.time()
     model.train_only_likelihood = False
-    
+
     pretraining_time = float(end_time - start_time)
-    
+    print(f"Pretraining took {pretraining_time} seconds.")
 
 # training
 trainer = pl.Trainer(
@@ -323,7 +338,28 @@ trainer = pl.Trainer(
                     deterministic=False,  # "warn",
                 )
 
+print("TRAINING THE MODEL!")
 
+start_time = time.time()
+trainer.fit(model, train_loader, validation_loader)
+end_time = time.time()
+
+training_time = float(end_time - start_time)
+print(f"Training took {training_time} seconds.")
+
+plot_training_results(
+    trainer = trainer,
+    pl_module = model,
+    estimated_beta = model.beta.detach().cpu().numpy(),
+    true_beta = model_gt_beta,
+    scale_l1 = model_scale_l1,
+    scale_kl = model_scale_kl,
+    scale_spectral = model_scale_spectral,
+    scale_lyapunov = model_scale_lyapunov,
+    file_name_plot = PLOTS_PATH.joinpath(run_id, "last.png"),
+    callback=True,
+    labels=None,
+)
 
 # log the environment
 current_time = time.strftime("%j%m%d%H%M%Z", time.gmtime())
