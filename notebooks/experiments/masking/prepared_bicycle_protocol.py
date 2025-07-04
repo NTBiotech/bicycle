e05f6ad61f6cdfba5aa8ed7582b6751b2c866973
NTBiotech <nicolas.toulouse@stud.uni-heidelberg.de>
2025-07-04 16:00:15 +0200

    Added support for create_data_from_grn

--- notebooks/experiments/masking/prepared_bicycle_protocol.py
+++ notebooks/experiments/masking/prepared_bicycle_protocol.py
@@ -19,7 +19,7 @@ from pytorch_lightning.loggers import Logger, CSVLogger
 import scanpy as sc
 
 
-from bicycle.utils.data import create_data, create_loaders, get_diagonal_mask
+from bicycle.utils.data import create_data, create_loaders, get_diagonal_mask, create_data_from_grn
 from bicycle.utils.general import get_id
 from bicycle.utils.plotting import plot_training_results
 from bicycle.callbacks import (
@@ -55,17 +55,17 @@ matmul_precision="high"
 trad_loading=True
 
 # masking
-masking_mode = None
+masking_mode = "loss"
 bin_prior = False
-scale_mask = 1
+scale_mask = 10
 parameter_set = "params5"
-grn_noise_factor= 2
+grn_noise_factor = 1
 normalize_mask = False
 # data
-create_new_data = True
+data_source = "create_data_from_grn"
 
 #scMultiSim
-data_id = "old_data/run_04"
+data_id = "data_run000"
 
 # training
 validation_size = 0.2
@@ -91,9 +91,6 @@ torch.set_float32_matmul_precision(matmul_precision)
 # configure output paths
 ## subdirectories model and plots
 ## in subdirectory data of the current path
-parameters_path = DATA_PATH/"parameters" / (parameter_set + ".pickle")
-
-
 print("Setting output paths...")
 
 OUT_PATH = DATA_PATH/"model_runs"
@@ -120,7 +117,7 @@ if not trad_loading:
     validation_size=0
 data_device=torch.device("cpu")
 
-if create_new_data:
+if data_source == "create_data":
     # create synthetic data
     ## parameters for data creation with prefix data
     print("Setting data creation parameters...")
@@ -170,27 +167,28 @@ if create_new_data:
         library_size_range = data_library_size_range,
         **data_graph_kwargs,
     )
-    # save data for later validation Commented for profiling TODO: uncomment
-    # print(f"Saving data at {str(MODELS_PATH.joinpath("synthetic_data"))}...")
-    '''MODELS_PATH.joinpath("synthetic_data").mkdir(parents=True, exist_ok=True)
+    # save data for later validation
+    print(f"Saving data at {str(MODELS_PATH.joinpath('synthetic_data'))}...")
+    MODELS_PATH.joinpath("synthetic_data").mkdir(parents=True, exist_ok=True)
     np.save(os.path.join(MODELS_PATH.joinpath("synthetic_data"), "check_sim_samples.npy"), samples)
     np.save(os.path.join(MODELS_PATH.joinpath("synthetic_data"), "check_sim_regimes.npy"), sim_regime)
     np.save(os.path.join(MODELS_PATH.joinpath("synthetic_data"), "check_sim_beta.npy"), beta)
     np.save(os.path.join(MODELS_PATH.joinpath("synthetic_data"), "check_sim_gt_interv.npy"), gt_interv)
-    '''
-    # TODO: create check for already existing runs
+
     # initialize data loaders
     print("Initializing Dataloaders...")
     
-    train_loader, validation_loader, test_loader = create_loaders(samples=samples, # test_loader is None
-                              validation_size=validation_size,
-                              batch_size=batch_size,
-                              SEED= SEED,
-                              train_gene_ko=data_train_gene_ko,
-                              test_gene_ko=data_test_gene_ko,
-                              persistent_workers=False,
-                              covariates=None,
-                              num_workers= loader_workers,
+    train_loader, validation_loader, test_loader = create_loaders(
+        samples=samples, # test_loader is None
+        sim_regime=sim_regime,
+        validation_size=validation_size,
+        batch_size=batch_size,
+        SEED= SEED,
+        train_gene_ko=data_train_gene_ko,
+        test_gene_ko=data_test_gene_ko,
+        persistent_workers=False,
+        covariates=None,
+        num_workers= loader_workers,
 
     )
     model_n_genes = samples.shape[1]
@@ -210,12 +208,13 @@ if create_new_data:
             bayes_prior = bayes_prior/np.max(bayes_prior)
         bayes_prior = torch.Tensor(bayes_prior)
 
-else:
+elif data_source == "scMultiSim":
     # get data from data_path
     # to define: samples (samples x genes), gt_interv (genes x contexts), sim_regime (intervened_variables = gt_interv[:, sim_regime].transpose(0, 1)), beta (n_genes x n_genes)
     
     # get gt_beta
     sms_path = DATA_PATH/"scMultiSim_data"/data_id
+    parameters_path = DATA_PATH/"parameters" / (parameter_set + ".pickle")
     grn = pd.read_csv(sms_path/"unperturbed_data"/"geff.csv", index_col=0)
     TFs = grn.columns.to_list()
     rna = sc.read_h5ad(sms_path/"ready_full_rna.h5ad")
@@ -244,18 +243,6 @@ else:
                 hard_mask[n] = np.zeros(possible_interactions.shape[0])
         # limit self interactions
         hard_mask*=np.diag(np.zeros(hard_mask.shape[0]))
-
-    if masking_loss:
-        values = grn.to_numpy()[grn>0]
-        grn_noise_var = np.std(values)
-        grn_noise_mean = np.mean(values)
-        grn_noise_p = get_sparsity(beta)*grn_noise_factor
-        salt = np.random.rand(*beta.shape) < grn_noise_p
-        bayes_prior = beta.copy()
-        bayes_prior[salt] = np.random.normal(loc=grn_noise_mean, scale=grn_noise_var, size=np.sum(salt))
-        if normalize_mask:
-            bayes_prior = bayes_prior/np.max(bayes_prior)
-        bayes_prior = torch.Tensor(bayes_prior)
 
     # get dataloaders
     dataloaders, gt_interv, sim_regime, mask = format_data(
@@ -288,6 +275,112 @@ else:
     else:
         model_init_tensors = {}
 
+    if masking_loss:
+        values = grn.to_numpy()[grn>0]
+        grn_noise_var = np.std(values)
+        grn_noise_mean = np.mean(values)
+        grn_noise_p = get_sparsity(beta)*grn_noise_factor
+        salt = np.random.rand(*beta.shape) < grn_noise_p
+        bayes_prior = beta.copy()
+        bayes_prior[salt] = np.random.normal(loc=grn_noise_mean, scale=grn_noise_var, size=np.sum(salt))
+        if normalize_mask:
+            bayes_prior = bayes_prior/np.max(bayes_prior)
+        bayes_prior = torch.Tensor(bayes_prior)
+
+elif data_source == "create_data_from_grn":
+    # create synthetic data
+    ## parameters for data creation with prefix data
+    print("Setting data creation parameters...")
+    
+    grn_path = DATA_PATH/"bicycle_data"/data_id
+    for p in grn_path.iterdir():
+        if p.name.endswith("GRN.csv"):
+            grn_path /= p.name
+    beta = pd.read_csv(grn_path, index_col=0).to_numpy()
+    data_n_genes=beta.shape[0]
+    data_n_samples_control = 8000
+    data_n_samples_per_perturbation = 400
+    data_train_gene_ko=list(np.arange(0, data_n_genes, 1).astype(str))
+    data_test_gene_ko = []
+    while len(data_test_gene_ko)<= data_n_genes*2:
+        data_test_gene_ko.append(tuple(np.random.choice(data_n_genes, size=2, replace=False).astype(str)))
+        data_test_gene_ko=list(set(data_test_gene_ko))
+    data_test_gene_ko = [f"{x[0]},{x[1]}" for x in data_test_gene_ko]
+    data_make_counts=True
+    data_sem="linear"
+    data_verbose=True
+    data_intervention_type="Cas9"
+    data_T=1.0
+    data_library_size_range=[10 * data_n_genes, 100 * data_n_genes]
+    data_graph_kwargs = {
+        "abs_weight_low": 0.25,
+        "abs_weight_high": 0.95,
+        "p_success": 0.5,
+        "expected_density": 2,
+        "noise_scale": 0.5,
+        "intervention_scale": 0.1,
+    }
+
+    print("Creating synthetic data...")
+    gt_dyn, intervened_variables, samples, gt_interv, sim_regime, beta=create_data_from_grn(
+        beta=beta,
+        n_samples_control=data_n_samples_control,
+        n_samples_per_perturbation=data_n_samples_per_perturbation,
+        train_gene_ko=data_train_gene_ko,
+        test_gene_ko=data_test_gene_ko,
+        verbose=data_verbose,
+        device=data_device,
+        T=data_T,
+        sem=data_sem,
+        intervention_type=data_intervention_type,
+        make_counts=data_make_counts,
+        library_size_range=data_library_size_range,
+        graph_kwargs=data_graph_kwargs,
+        )
+
+    # save data for later validation
+    print(f"Saving data at {str(MODELS_PATH.joinpath('synthetic_data'))}...")
+    MODELS_PATH.joinpath("synthetic_data").mkdir(parents=True, exist_ok=True)
+    np.save(os.path.join(MODELS_PATH.joinpath("synthetic_data"), "check_sim_samples.npy"), samples)
+    np.save(os.path.join(MODELS_PATH.joinpath("synthetic_data"), "check_sim_regimes.npy"), sim_regime)
+    np.save(os.path.join(MODELS_PATH.joinpath("synthetic_data"), "check_sim_beta.npy"), beta)
+    np.save(os.path.join(MODELS_PATH.joinpath("synthetic_data"), "check_sim_gt_interv.npy"), gt_interv)
+    
+
+    # initialize data loaders
+    print("Initializing Dataloaders...")
+    
+    train_loader, validation_loader, test_loader = create_loaders(
+        samples=samples, # test_loader is None
+        sim_regime=sim_regime,
+        validation_size=validation_size,
+        batch_size=batch_size,
+        SEED= SEED,
+        train_gene_ko=data_train_gene_ko,
+        test_gene_ko=data_test_gene_ko,
+        persistent_workers=False,
+        covariates=None,
+        num_workers= loader_workers,
+    )
+    model_n_genes = samples.shape[1]
+    model_n_samples = samples.shape[0]
+    model_train_gene_ko = data_train_gene_ko
+    model_test_gene_ko = data_test_gene_ko
+    model_init_tensors = {}
+    if masking_loss:
+        values = gt_dyn[gt_dyn>0]
+        grn_noise_var = np.std(values)
+        grn_noise_mean = np.mean(values)
+        grn_noise_p = get_sparsity(gt_dyn)*grn_noise_factor
+        salt = np.random.rand(*gt_dyn.shape) < grn_noise_p
+        bayes_prior = gt_dyn.copy()
+        bayes_prior[salt] = np.random.normal(loc=grn_noise_mean, scale=grn_noise_var, size=np.sum(salt))
+        if normalize_mask:
+            bayes_prior = bayes_prior/np.max(bayes_prior)
+        bayes_prior = torch.Tensor(bayes_prior)
+
+else:
+    raise ValueError(f"Invalid data_source: {data_source}. Choose one of ['create_data','create_data_from_grn','scMultiSim']!")
 
 # initialize parameters for the bicycle class with prefix model
 print("Initializing model parameters...")
